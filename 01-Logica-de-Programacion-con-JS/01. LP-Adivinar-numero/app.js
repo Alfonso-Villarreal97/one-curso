9c460ed76e79cdabb0a7f776b73f53dd4a3a426b
Alfonso-Villarreal97 <dtrejov97@gmail.com>
2024-01-24 18:03:26 -0600

    Edicion final para curso Logica de Programación, desafío final del curso resuelto

--- 01-Logica-de-Programacion-con-JS/01. LP-Adivinar-numero/app.js
+++ 01-Logica-de-Programacion-con-JS/01. LP-Adivinar-numero/app.js
@@ -1,39 +1,49 @@
+//Desafío final
+//  ToDo:
+//  ¿Qué cambios serían necesarios para que no sea entre 1 y 10? Imaginen que quieran jugar entre 1 y 100, o que el usuario quiera jugar entre 1 y 1000.
+//  ¿Qué cambios serían necesarios para que esta funcionalidad pueda ser implementada, pueda ser aplicada?
+
 //Variables
-let numeroMaximoPosible = 100;
-let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
 let numeroUsuario = 0;
 let intentos = 1;
-//let palabraVeces = 'vez';
 let maximosIntentos = 6;
 
+// Validamos que el numero del usuario sea > a 1 y si no ingresa un numero válido, entramos en un bucle hasta que elija otro numero
+let numeroMaximoPosible = parseInt(prompt('Indica el número máximo de números en los que te gustaría jugar'));
+while (numeroMaximoPosible <= 1) {
+    numeroMaximoPosible = prompt('Ingresa un valor adecuado para jugar (mayor a 1)')
+}
 
-while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`));
+//Continua el flujo, solicitamos que el usuario ingrese un numero dentro de 1 hasta el rango que el eligió
+numeroUsuario = parseInt(prompt(`Juego iniciado, indica un número entre 1 y tu numero que fue ->${numeroMaximoPosible}<- por favor:`));
+let numeroSecreto = Math.floor(Math.random() * numeroMaximoPosible) + 1;
 
-    console.log(typeof(numeroUsuario));
+while (numeroUsuario != numeroSecreto) {    
+
     if (numeroUsuario == numeroSecreto) {
         //Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
     } else {
+        //No acertamos y le pedimos al usuario que
         if (numeroUsuario > numeroSecreto) {
-            alert('El número secreto es menor');
+            alert(`El número secreto es menor a ${numeroUsuario}`);
+            numeroUsuario = parseInt(prompt(`Indica un número entre 1 y tu numero que fue ->${numeroMaximoPosible}<- por favor:`)); //Reasignamos variable
         } else {
-            alert('El número secreto es mayor');
-        }
-        //Incrementamos el contador cuando no acierta
-        //intentos = intentos + 1;
-        //intentos += 1;
-        intentos++;
-
-        //palabraVeces = 'veces';
+            alert(`El número secreto es mayor a ${numeroUsuario}`);
+            numeroUsuario = parseInt(prompt(`Indica un número entre 1 y tu numero que fue ->${numeroMaximoPosible}<- por favor:`)); //Reasignamos variable
+        }       
+        intentos++;     
         if (intentos > maximosIntentos) {
             alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            alert(`El número secreto era: ${numeroSecreto}`);
             break;
         }
-        //La condición no se cumplió
-        //alert('Lo siento, no acertaste el número');
     }
 }
+
+
+
+
 
 
 
